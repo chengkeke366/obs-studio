754b04f8580f234cb9dee36d165bf43dd918ec98
chengkeke <chengkeke@xdf.cn>
2022-08-10 15:05:02 +0800

    26.1.2 代码学习用

---
+++ build.bat
@@ -0,0 +1,8 @@
+set Current_DIR=%cd%
+set DepsPath=%Current_DIR%\despence_resource\dependencies2019\win64
+set QTDIR=C:\Qt\Qt5.14.2\5.14.2\msvc2017_64
+set CEF_ROOT_DIR=%Current_DIR%\despence_resource\cef_binary_75.1.16+g16a67c4+chromium-75.0.3770.100_windows64_minimal
+"C:\Program Files\CMake\bin\cmake-gui.exe"
+
+
+
